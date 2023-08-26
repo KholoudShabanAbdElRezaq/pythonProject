3bc9af6226e37f72407e015f1788756e32b1a44b
kholoud_shaban_aman- <KholoudShaban53@gmail.com>
2023-08-26 13:11:08 +0300

    commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import csv
+
+x = pd.read_csv('/Users/user/Downloads/Employees.csv')
+without_dup = x.drop_duplicates()
+print(without_dup)
+without_dup['Age'] = without_dup['Age'].round(0)
+print("The Age Without Decimal Point")
+print(without_dup)
+var = without_dup['Age']
+print('The Average Of Age ')
+print(sum(var / len(var)))
+varr = without_dup['Salary(USD)']
+v = without_dup['Salary(USD)'] = varr.median()
+print("The Median Salaries")
+print(v)
+print("Ration between males and female employees")
+print(f"M : {len(without_dup[without_dup['Gender'] == 'M'])}")
+print(f"F : {len(without_dup[without_dup['Gender'] == 'F'])}")
+
+file = open('/Users/user/Downloads/Employee.csv')
+writer = csv.writer(file)
+writer.writerows(without_dup)
+file.close()
